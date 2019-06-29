c970b95aeccc8f1a357c7784674e462c32d43555
razdvapoka <razdvapoka@gmail.com>
2019-06-29 20:01:22 +0300

    :bento: Add missing assets

---
+++ src/components/icons/NotDef.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SvgNotDef = props => (
+  <svg data-name='Layer 1' viewBox='0 0 50 50' {...props}>
+    <path d='M25 4.92A20.08 20.08 0 1 0 45.08 25 20.08 20.08 0 0 0 25 4.92zm0 26.66A6.58 6.58 0 1 1 31.58 25 6.59 6.59 0 0 1 25 31.58z' />
+    <circle cx={25} cy={25} r={2} />
+  </svg>
+)
+
+export default SvgNotDef
